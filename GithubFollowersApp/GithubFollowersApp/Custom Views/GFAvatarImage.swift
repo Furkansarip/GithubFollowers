65417fc8753c10266590056cfaf1c95c7cef4497
Furkan Sarı <furkansarip@gmail.com>
2022-09-25 18:25:01 +0300

    Custom Follower Cell

---
+++ GithubFollowersApp/GithubFollowersApp/Custom Views/GFAvatarImage.swift
@@ -0,0 +1,29 @@
+//
+//  GFAvatarImage.swift
+//  GithubFollowersApp
+//
+//  Created by Furkan Sarı on 25.09.2022.
+//
+
+import UIKit
+
+class GFAvatarImage: UIImageView {
+
+    
+    let placeholder = UIImage(named: "avatar-placeholder-dark")
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        layer.cornerRadius = 10
+        image = placeholder
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
