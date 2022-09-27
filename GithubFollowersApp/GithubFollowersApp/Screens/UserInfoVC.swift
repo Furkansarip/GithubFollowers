5de60512ed72ac215d6e8b3affa99e22e4dc6518
Furkan Sarı <furkansarip@gmail.com>
2022-09-27 23:29:42 +0300

    Single User Network Call

--- GithubFollowersApp/GithubFollowersApp/Screens/UserInfoVC.swift
+++ GithubFollowersApp/GithubFollowersApp/Screens/UserInfoVC.swift
@@ -16,7 +16,14 @@ class UserInfoVC: UIViewController {
         
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action:#selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
-        print(username)
+        NetworkManager.shared.getUserInfo(for: username) {[weak self] result in
+            guard let self = self else {return}
+            switch result {
+            case .success(let user):
+                print(user)
+            case.failure(let error):
+                self.GFAlertPresent(title: "Something wrong!", message: error.rawValue, buttonTitle: "OK")            }
+        }
     }
     
     @objc func dismissVC(){
